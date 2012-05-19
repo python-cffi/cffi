50ac9b7572f9f42647bfea4c7ef4a09aa19d967c
Armin Rigo <arigo@tunes.org>
2012-05-19 19:55:28 +0200

    A passing test.

--- testing/test_math.py
+++ testing/test_math.py
@@ -10,3 +10,14 @@ def test_sin():
     m = ffi.load("m")
     x = m.sin(1.23)
     assert x == math.sin(1.23)
+
+def test_sinf():
+    ffi = FFI()
+    ffi.cdef("""
+        float sinf(float x);
+    """)
+    m = ffi.load("m")
+    x = m.sinf(1.23)
+    assert type(x) is float
+    assert x != math.sin(1.23)    # rounding effects
+    assert abs(x - math.sin(1.23)) < 1E-6
