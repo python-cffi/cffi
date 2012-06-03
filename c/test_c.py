1bd7d766f9ad55e05a6d7ed4fab234674f0f5cea
Armin Rigo <arigo@tunes.org>
2012-06-03 21:30:51 +0200

    ffi.gc(), just because it's easy here.

--- c/test_c.py
+++ c/test_c.py
@@ -705,3 +705,16 @@ def test_bitfield_instance_init():
     BStruct = new_struct_type("foo")
     complete_struct_or_union(BStruct, [('a1', BInt, 1)])
     py.test.raises(NotImplementedError, new, new_pointer_type(BStruct), [-1])
+
+def test_gc():
+    from gc import collect
+    BInt = new_primitive_type("int")
+    n = cast(BInt, 123)
+    py.test.raises(TypeError, gc, n, 5)
+    destroyed = []
+    m = gc(n, destroyed.append)
+    assert repr(m) == "<cdata 'int' with destructor>"
+    assert destroyed == []
+    del m; collect()
+    assert len(destroyed) == 1
+    assert int(destroyed[0]) == 123
