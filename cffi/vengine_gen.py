146dc4079ba77eda78b8b837bfa506af31dc5125
Armin Rigo <arigo@tunes.org>
2012-08-11 21:11:42 +0200

    Fix for test_vgen:test_nonstandard_integer_types on Windows.

--- cffi/vengine_gen.py
+++ cffi/vengine_gen.py
@@ -438,10 +438,20 @@ cffimod_header = r'''
 #include <stddef.h>
 #include <stdarg.h>
 #include <errno.h>
+#include <sys/types.h>   /* XXX for ssize_t on some platforms */
+
 #ifdef _WIN32
 #  include <Windows.h>
+typedef __int8 int8_t;
+typedef __int16 int16_t;
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+typedef SSIZE_T ssize_t;
 #else
 #  include <stdint.h>
-#  include <sys/types.h>   /* XXX for ssize_t */
 #endif
 '''
