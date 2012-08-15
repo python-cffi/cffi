501392517ae76fc6fb35f4964d2bbd79047b9cab
Armin Rigo <arigo@tunes.org>
2012-08-15 16:46:33 +0200

    pycparser 2.08 has been fixed.  Thanks!

--- setup.py
+++ setup.py
@@ -95,8 +95,6 @@ if __name__ == '__main__':
     },
 
     install_requires=[
-        # pycparser 2.08 no longer contains lextab.py/yacctab.py
-        # out of the box, which looks like a bug
-        'pycparser<=2.07',
+        'pycparser',
     ]
   )
