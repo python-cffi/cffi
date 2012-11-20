5478ff17b9043d8e5c10249c50a5fe250761a755
Armin Rigo <arigo@tunes.org>
2012-11-20 23:50:54 +0100

    Bah, the warning was about the previous line.

--- c/misc_win32.h
+++ c/misc_win32.h
@@ -125,7 +125,7 @@ typedef unsigned __int8 uint8_t;
 typedef unsigned __int16 uint16_t;
 typedef unsigned __int32 uint32_t;
 typedef unsigned __int64 uint64_t;
-typedef SSIZE_T ssize_t;
+typedef unsigned char _Bool;
 
 
 /************************************************************/
