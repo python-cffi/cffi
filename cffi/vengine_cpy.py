6348d3e44159ca348506811ace7d6f501b9af2a8
Armin Rigo <arigo@tunes.org>
2013-01-07 00:28:34 +0100

    Win32: needs this include now.

--- cffi/vengine_cpy.py
+++ cffi/vengine_cpy.py
@@ -701,6 +701,7 @@ cffimod_header = r'''
 #include <stddef.h>
 
 #ifdef MS_WIN32
+#include <malloc.h>   /* for alloca() */
 typedef __int8 int8_t;
 typedef __int16 int16_t;
 typedef __int32 int32_t;
