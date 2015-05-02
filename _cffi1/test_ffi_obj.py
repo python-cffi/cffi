f4bef904fd5cf77ea657fd5b19e154f0a5ce9c7d
Armin Rigo <arigo@tunes.org>
2015-05-02 22:12:11 +0200

    Check that the base class' instances don't allow random attributes

--- _cffi1/test_ffi_obj.py
+++ _cffi1/test_ffi_obj.py
@@ -55,6 +55,12 @@ def test_ffi_NULL():
     NULL = _cffi1_backend.FFI.NULL
     assert _cffi1_backend.FFI().typeof(NULL).cname == "void *"
 
+def test_ffi_no_attr():
+    ffi = _cffi1_backend.FFI()
+    py.test.raises(AttributeError, "ffi.no_such_name")
+    py.test.raises(AttributeError, "ffi.no_such_name = 42")
+    py.test.raises(AttributeError, "del ffi.no_such_name")
+
 def test_ffi_string():
     ffi = _cffi1_backend.FFI()
     p = ffi.new("char[]", b"foobar\x00baz")
