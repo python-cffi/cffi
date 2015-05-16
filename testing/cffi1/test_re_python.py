8109bd3950304c96da617d7e46f95817e0236667
Armin Rigo <arigo@tunes.org>
2015-05-16 10:24:38 +0200

    more tests

--- testing/cffi1/test_re_python.py
+++ testing/cffi1/test_re_python.py
@@ -1,4 +1,5 @@
 import sys
+import py
 from cffi import FFI
 from cffi import recompiler, ffiplatform
 from testing.udir import udir
@@ -8,6 +9,8 @@ def setup_module(mod):
     SRC = """
     #define FOOBAR (-42)
     int add42(int x) { return x + 42; }
+    struct foo_s;
+    struct bar_s { int x; signed char a[]; };
     """
     tmpdir = udir.join('test_re_python')
     tmpdir.ensure(dir=1)
@@ -21,6 +24,8 @@ def setup_module(mod):
     ffi.cdef("""
     #define FOOBAR -42
     int add42(int);
+    struct foo_s;
+    struct bar_s { int x; signed char a[]; };
     """)
     ffi.set_source('re_python_pysrc', None)
     ffi.emit_python_code(str(tmpdir.join('re_python_pysrc.py')))
@@ -36,3 +41,20 @@ def test_function():
     from re_python_pysrc import ffi
     lib = ffi.dlopen(extmod)
     assert lib.add42(-10) == 32
+
+def test_constant_via_lib():
+    from re_python_pysrc import ffi
+    lib = ffi.dlopen(extmod)
+    assert lib.FOOBAR == -42
+
+def test_opaque_struct():
+    from re_python_pysrc import ffi
+    ffi.cast("struct foo_s *", 0)
+    py.test.raises(TypeError, ffi.new, "struct foo_s *")
+
+def test_nonopaque_struct():
+    from re_python_pysrc import ffi
+    p = ffi.new("struct bar_s *", [5, "foobar"])
+    assert p.x == 5
+    assert p.a[0] == ord('f')
+    assert p.a[5] == ord('r')
