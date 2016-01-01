f49fbc62bce1efbe08af6809b13aedda013dbcae
Armin Rigo <arigo@tunes.org>
2016-01-01 11:45:42 +0100

    test for not invoking the init code recursively

---
+++ testing/embedding/add_recursive.py
@@ -0,0 +1,26 @@
+import cffi
+
+ffi = cffi.FFI()
+
+ffi.cdef("""
+    int (*my_callback)(int);
+    extern "Python" int add_rec(int, int);
+""", dllexport=True)
+
+ffi.embedding_init_code(r"""
+    print "preparing REC"
+
+    @ffi.def_extern()
+    def add_rec(x, y):
+        print "adding %d and %d" % (x, y)
+        return x + y
+
+    x = lib.my_callback(400)
+    print '<<< %d >>>' % (x,)
+""")
+
+ffi.set_source("_add_recursive_cffi", """
+int (*my_callback)(int);
+""")
+
+ffi.compile()
