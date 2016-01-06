7cd4510555d7d2238e2bd05268c131a809bb3985
Armin Rigo <arigo@tunes.org>
2016-01-06 21:28:03 +0100

    Check that the module is present in sys.modules at this point, and that it is re-importable

--- testing/embedding/add2.py
+++ testing/embedding/add2.py
@@ -10,6 +10,13 @@ ffi.embedding_init_code(r"""
     import sys
     sys.stdout.write("prepADD2\n")
 
+    assert '_add2_cffi' in sys.modules
+    m = sys.modules['_add2_cffi']
+    import _add2_cffi
+    assert m is _add2_cffi
+    assert _add2_cffi.ffi is ffi
+    assert _add2_cffi.lib is lib
+
     @ffi.def_extern()
     def add2(x, y, z):
         sys.stdout.write("adding %d and %d and %d\n" % (x, y, z))
