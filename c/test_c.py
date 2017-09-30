4852512bc373bdd1418f5946f9c905e04ad73062
Armin Rigo <arigo@tunes.org>
2017-09-30 08:34:35 +0200

    ImportError

--- c/test_c.py
+++ c/test_c.py
@@ -2110,6 +2110,7 @@ def test_wchar():
     if sys.platform.startswith("linux"):
         BWChar = new_primitive_type("wchar_t")
         assert sizeof(BWChar) == 4
+        import platform
         if platform.machine().startswith(('arm', 'aarch64')):
             assert int(cast(BWChar, -1)) == 4294967295 # unsigned, on ARM
         else:
