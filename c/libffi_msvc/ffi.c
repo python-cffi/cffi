58fbe7343ffa341c02accd0de771502b2e8ffff9
Armin Rigo <arigo@tunes.org>
2020-07-21 20:14:11 +0200

    next fix, for win64

--- c/libffi_msvc/ffi.c
+++ c/libffi_msvc/ffi.c
@@ -342,6 +342,8 @@ ffi_closure_SYSV (ffi_closure *closure, char *argp)
      result types except for floats; we have to 'mov xmm0, rax' in the
      caller to correct this.
   */
+  if (rtype == FFI_TYPE_STRUCT)
+      return resp;
   return *(void **)resp;
 #endif
 }
