268d7f43e3302c41ddad84b8be0a984e9871f875
Miro Hrončok <miro@hroncok.cz>
2022-11-14 12:30:12 +0100

    Drop py.code usage from tests, no longer depend on the deprecated py package

--- testing/cffi1/test_dlopen_unicode_literals.py
+++ testing/cffi1/test_dlopen_unicode_literals.py
@@ -1,4 +1,4 @@
-import py, os
+import os
 
 s = """from __future__ import unicode_literals
 """
@@ -6,4 +6,4 @@ s = """from __future__ import unicode_literals
 with open(os.path.join(os.path.dirname(__file__), 'test_dlopen.py')) as f:
     s += f.read()
 
-exec(py.code.compile(s))
+exec(compile(s, filename='test_dlopen.py', mode='exec'))
