57ff08e2b693858077740ef1b1a0278dc6ed6de1
Matt Davis <mrd@redhat.com>
2023-09-25 12:15:10 -0700

    remove obsolete test
    (version no longer listed in installation.rst)

--- testing/cffi0/test_version.py
+++ testing/cffi0/test_version.py
@@ -33,18 +33,6 @@ def test_doc_version():
     assert ("version = '%s'\n" % v[:4]) in content
     assert ("release = '%s'\n" % v) in content
 
-def test_doc_version_file():
-    parent = os.path.dirname(os.path.dirname(cffi.__file__))
-    v = cffi.__version__.replace('+', '')
-    p = os.path.join(parent, 'doc', 'source', 'installation.rst')
-    content = open(p).read()
-    if " package version %s:" % v not in content:
-        for i in range(5):
-            if " package version %s-%d:" % (v, i) in content:
-                break
-        else:
-            assert 0, "doc/source/installation.rst needs updating"
-
 def test_setup_version():
     parent = os.path.dirname(os.path.dirname(cffi.__file__))
     p = os.path.join(parent, 'setup.py')
