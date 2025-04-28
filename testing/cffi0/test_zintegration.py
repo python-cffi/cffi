d85a104ea8f0e4f909bf884754353f80d8e4d134
Stefano Rivera <stefano@rivera.za.net>
2025-04-28 12:04:37 -0400

    Explicitly specify --no-build-isolation as that's our expectation in these tests
    Previously we relied on pip to build the packages in non-PEP517 mode,
    which implied no build isolation.
    
    The latest `virtualenv` (with pypa/virtualenv#2868) won't include
    `wheel` in the virtualenv, which will mean that pip uses PEP-517 mode,
    which is isolated by default.

--- testing/cffi0/test_zintegration.py
+++ testing/cffi0/test_zintegration.py
@@ -98,7 +98,7 @@ def really_run_setup_and_program(dirname, venv_dir_and_paths, python_snippet):
         # there's a setuptools/easy_install bug that causes this to fail when the build/install occur together and
         # we're in the same directory with the build (it tries to look up dependencies for itself on PyPI);
         # subsequent runs will succeed because this test doesn't properly clean up the build- use pip for now.
-        subprocess.check_call((vp, '-m', 'pip', 'install', '.'), env=env)
+        subprocess.check_call((vp, '-m', 'pip', 'install', '.', '--no-build-isolation'), env=env)
         subprocess.check_call((vp, str(python_f)), env=env)
     finally:
         os.chdir(olddir)
