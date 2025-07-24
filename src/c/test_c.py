277ae403ecf8bdc1223d6155c3ca81b1f4d9846a
Łukasz Langa <lukasz@langa.pl>
2025-07-24 17:00:24 +0200

    Add support for building ARM64 iOS wheels on CI

--- src/c/test_c.py
+++ src/c/test_c.py
@@ -17,6 +17,9 @@ if sys.platform == 'linux':
     except ImportError:
         pass
 
+is_ios = sys.platform == 'ios'
+
+
 def _setup_path():
     import os, sys
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
@@ -1229,6 +1232,10 @@ def test_cannot_pass_struct_with_array_of_length_0():
     BFunc2 = new_function_type((BInt,), BStruct, False)
     pytest.raises(NotImplementedError, cast(BFunc2, 123), 123)
 
+@pytest.mark.xfail(
+    is_ios,
+    reason="For an unknown reason f(1, cast(BInt, 42)) returns 36792864",
+)
 def test_call_function_9():
     BInt = new_primitive_type("int")
     BFunc9 = new_function_type((BInt,), BInt, True)    # vararg
@@ -1360,6 +1367,7 @@ def test_write_variable():
     pytest.raises(ValueError, ll.write_variable, BVoidP, "stderr", stderr)
 
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback():
     BInt = new_primitive_type("int")
     def make_callback():
@@ -1376,6 +1384,7 @@ def test_callback():
     assert str(e.value) == "'int(*)(int)' expects 1 arguments, got 0"
 
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_exception():
     def check_value(x):
         if x == 10000:
@@ -1432,6 +1441,7 @@ def test_callback_exception():
         assert ff(bigvalue) == -42
 
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_return_type():
     for rettype in ["signed char", "short", "int", "long", "long long",
                     "unsigned char", "unsigned short", "unsigned int",
@@ -1452,6 +1462,7 @@ def test_callback_return_type():
         assert f(max - 1) == max
         assert f(max) == 42
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_a_lot_of_callbacks():
     BIGNUM = 10000
     if 'PY_DOT_PY' in globals(): BIGNUM = 100   # tests on py.py
@@ -1467,6 +1478,7 @@ def test_a_lot_of_callbacks():
     for i, f in enumerate(flist):
         assert f(-142) == -142 + i
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_receiving_tiny_struct():
     BSChar = new_primitive_type("signed char")
     BInt = new_primitive_type("int")
@@ -1482,6 +1494,7 @@ def test_callback_receiving_tiny_struct():
     n = f(p[0])
     assert n == -42
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_returning_tiny_struct():
     BSChar = new_primitive_type("signed char")
     BInt = new_primitive_type("int")
@@ -1499,6 +1512,7 @@ def test_callback_returning_tiny_struct():
     assert s.a == -10
     assert s.b == -30
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_receiving_struct():
     BSChar = new_primitive_type("signed char")
     BInt = new_primitive_type("int")
@@ -1515,6 +1529,7 @@ def test_callback_receiving_struct():
     n = f(p[0])
     assert n == 42
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_returning_struct():
     BSChar = new_primitive_type("signed char")
     BInt = new_primitive_type("int")
@@ -1534,6 +1549,7 @@ def test_callback_returning_struct():
     assert s.a == -10
     assert s.b == 1E-42
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_receiving_big_struct():
     BInt = new_primitive_type("int")
     BStruct = new_struct_type("struct foo")
@@ -1558,6 +1574,7 @@ def test_callback_receiving_big_struct():
     n = f(p[0])
     assert n == 42
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_returning_big_struct():
     BInt = new_primitive_type("int")
     BStruct = new_struct_type("struct foo")
@@ -1583,6 +1600,7 @@ def test_callback_returning_big_struct():
     for i, name in enumerate("abcdefghij"):
         assert getattr(s, name) == 13 - i
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_returning_void():
     BVoid = new_void_type()
     BFunc = new_function_type((), BVoid, False)
@@ -1691,6 +1709,7 @@ def test_enum_overflow():
                     pytest.raises(OverflowError, new_enum_type,
                                    "foo", ("AA",), (testcase,), BPrimitive)
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_returning_enum():
     BInt = new_primitive_type("int")
     BEnum = new_enum_type("foo", ('def', 'c', 'ab'), (0, 1, -20), BInt)
@@ -1707,6 +1726,7 @@ def test_callback_returning_enum():
     assert f(20) == 20
     assert f(21) == 21
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_returning_enum_unsigned():
     BInt = new_primitive_type("int")
     BUInt = new_primitive_type("unsigned int")
@@ -1724,6 +1744,7 @@ def test_callback_returning_enum_unsigned():
     assert f(20) == 20
     assert f(21) == 21
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_returning_char():
     BInt = new_primitive_type("int")
     BChar = new_primitive_type("char")
@@ -1738,6 +1759,7 @@ def _hacked_pypy_uni4():
     pyuni4 = {1: True, 2: False}[len(u+'\U00012345')]
     return 'PY_DOT_PY' in globals() and not pyuni4
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_callback_returning_wchar_t():
     BInt = new_primitive_type("int")
     BWChar = new_primitive_type("wchar_t")
@@ -2307,6 +2329,8 @@ def _test_wchar_variant(typename):
     assert str(q) == repr(q)
     pytest.raises(RuntimeError, string, q)
     #
+    if is_ios:
+        return  # cannot allocate executable memory for the callback() below
     def cb(p):
         assert repr(p).startswith("<cdata '%s *' 0x" % typename)
         return len(string(p))
@@ -2543,6 +2567,7 @@ def test_errno():
     f(); f()
     assert get_errno() == 95
 
+@pytest.mark.skipif(is_ios, reason="Cannot allocate executable memory on iOS")
 def test_errno_callback():
     if globals().get('PY_DOT_PY'):
         pytest.skip("cannot run this test on py.py (e.g. fails on Windows)")
@@ -2993,6 +3018,11 @@ if sys.version_info >= (3,):
     except ImportError:
         pass   # win32
 
+@pytest.mark.skip(
+    is_ios,
+    reason="For an unknown reason fscanf() doesn't read anything on 3.14"
+           " and crashes on 3.13 (that's why it's not an xfail)",
+)
 def test_FILE():
     if sys.platform == "win32":
         pytest.skip("testing FILE not implemented")
