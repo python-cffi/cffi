9ee453352e1ed98cad9d444f3473a000b301e39e
Thomas A Caswell <tcaswell@gmail.com>
2025-11-02 17:05:42 -0500

    BLD: catch the rest of the places the version is hard-coded

--- src/cffi/__init__.py
+++ src/cffi/__init__.py
@@ -5,7 +5,7 @@ from .api import FFI
 from .error import CDefError, FFIError, VerificationError, VerificationMissing
 from .error import PkgConfigError
 
-__version__ = "2.0.0.dev0"
+__version__ = "2.0.1.dev0"
 __version_info__ = (2, 0, 0, 'dev0')
 
 # The verifier module file names are based on the CRC32 of a string that
