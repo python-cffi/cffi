d443106feb8faf4b6bef038bcb5cf821f3aa202b
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-11-22 15:40:48 +0100

    Apply ruff/pycodestyle rule E711
    E711 Comparison to `None` should be `cond is not None`

--- testing/cffi0/backend_tests.py
+++ testing/cffi0/backend_tests.py
@@ -207,7 +207,7 @@ class BackendTests:
         assert p is not None
         assert bool(p) is False
         assert p == ffi.cast("int*", 0)
-        assert p != None
+        assert p is not None
         assert repr(p) == "<cdata 'int *' NULL>"
         a = ffi.new("int[]", [123, 456])
         p = ffi.cast("int*", a)
@@ -397,7 +397,7 @@ class BackendTests:
         ffi = FFI(backend=self.Backend())
         p = ffi.new("int*[1]")
         assert p[0] is not None
-        assert p[0] != None
+        assert p[0] is not None
         assert p[0] == ffi.NULL
         assert repr(p[0]) == "<cdata 'int *' NULL>"
         #
@@ -1154,14 +1154,14 @@ class BackendTests:
         assert (p >  q) is False
         assert (p >= q) is False
         #
-        assert (None == s) is False
-        assert (None != s) is True
-        assert (s == None) is False
-        assert (s != None) is True
-        assert (None == q) is False
-        assert (None != q) is True
-        assert (q == None) is False
-        assert (q != None) is True
+        assert (None is s) is False
+        assert (None is not s) is True
+        assert (s is None) is False
+        assert (s is not None) is True
+        assert (None is q) is False
+        assert (None is not q) is True
+        assert (q is None) is False
+        assert (q is not None) is True
 
     def test_integer_comparison(self):
         ffi = FFI(backend=self.Backend())
